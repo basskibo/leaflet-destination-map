8f6e7ecb886b92314a1c29d616b05bfe1d92c29d
Bojan Jagetic <jagetic.bojan@gmail.com>
2024-10-04 17:25:31 +0200

    feat: zoom actions added

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,10 @@ router.get('/', function(req, res, next) {
 router.get('/destinations', (req, res) => {
     const destinations = [
         { name: "Paris", lat: 48.8566, lon: 2.3522 },
-        // { name: "New York", lat: 40.7128, lon: -74.0060 },
+        { name: "New York", lat: 40.7128, lon: -74.0060 },
 		{ name: 'Novi Sad', lat: 45.2398, lon: 19.8227},
-        // { name: "Tokyo", lat: 35.6762, lon: 139.6503 },
+        { name: "Tokyo", lat: 35.6762, lon: 139.6503 },
+        { name: "Bangkok", lat: 13.6854, lon: 100.6114 }, //13.685448106045627, 100.61142515277689
     ];
     res.json(destinations);
 });
